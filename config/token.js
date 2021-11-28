032cdcf49e8dec31e5269f033a21daa6812f8fb7
Josehpequeno <hicarojbs21@gmail.com>
2021-11-28 16:24:32 -0300

    mudanças em token e preview image

--- config/token.js
+++ config/token.js
@@ -1,10 +1,19 @@
-var colors = require('colors');
+const colors = require('colors');
+const db = require('./db');
+const MangaDao = require('../dao/manga-dao');
+let mangaDao = new MangaDao(db);
 require('dotenv/config');
 
-const jwt = require('jsonwebtoken'); //token
-//* Token para o uso da api.
-const token = jwt.sign({ id: 0 }, process.env.SECRET, {
-    expiresIn: 300 // expires in 5min
-});
-console.log("Generated token for API:");
-console.log(colors.rainbow(token)+'\n');
+mangaDao.getAllMangas().then(results => {
+    console.log("Mangas: ");
+    results.forEach(element => {
+        console.log(colors.yellow("## " + element.name));
+    })
+    const jwt = require('jsonwebtoken'); //token
+    //* Token para o uso da api.
+    const token = jwt.sign({ id: 0 }, process.env.SECRET, {
+        expiresIn: 1200 // expires in 20min
+    });
+    console.log("\nGenerated token for API:");
+    console.log(colors.rainbow(token) + '\n');
+}).catch(err => console.error(err));
